2f2af65ed8677996faab40b544e02da85aa84011
vitekma1 <matejvitek@seznam.cz>
2020-01-28 13:49:50 +0100

    pocet vstupu za urcity mesic

--- src/main/java/cz/ppro/gymapp/be/model/Statistics.java
+++ src/main/java/cz/ppro/gymapp/be/model/Statistics.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+
 @Entity
 @Table(name="statistics")
 public class Statistics {
@@ -15,7 +17,6 @@ public class Statistics {
     private Long id;
     @ManyToOne
     private Account account;
-
 
     public Statistics(Account account) {
         this.account = account;
@@ -122,8 +123,110 @@ public class Statistics {
         return count;
     }
 
-    // nejoblibenejsi kurz
-    // nejoblibenejsi den/cas
+    //pocet vstupu za poslednich 7 dni
+    public int getCountOfEntrancesOfLastSevenDays(){
+        int count=0;
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_MONTH, -7);
+        date = calendar.getTime();
+        for (int i=0; i<account.getTickets().size(); i++)
+        {
+            for (int j=0; j<account.getTickets().get(i).getEntrances().size(); j++)
+            {
+                if(account.getTickets().get(i).getEntrances().get(j).getBeginDate().equals(date)) {
+                    count++;
+                }
+            }}
+        return count;
+    }
 
+    //celkovy pocet vstupu vsech zakazniku v zadany den
+    public int getCountOFEndtrances(List<Entrance> entrances, Date day){
+        int count=0;
+        for (int i=0; i<entrances.size(); i++)
+        {
+            if(entrances.get(i).getBeginDate().equals(day)) {
+                count++;
+            }
+
+            }
+        return count;
+    }
+
+    // nejoblibenejsi den
+    public String getFavouriteDay(List<Entrance> entrances){
+        int mondayCount=0;
+        int tuesdayCount=0;
+        int thursdayCount=0;
+        int wednesdayCount=0;
+        int fridayCount=0;
+        int saturdayCount=0;
+        int sundayCount=0;
+        for (int i=0; i<entrances.size(); i++)
+        {
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(entrances.get(i).getBeginDate());
+            boolean monday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY;
+            boolean tuesday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.TUESDAY;
+            boolean thursday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.THURSDAY;
+            boolean wednesday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.WEDNESDAY;
+            boolean friday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.FRIDAY;
+            boolean saturday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY;
+            boolean sunday = calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY;
+            if(monday) {
+                mondayCount++;
+            }
+            if(tuesday) {
+                tuesdayCount++;
+            }
+            if(thursday) {
+                thursdayCount++;
+            }
+            if(wednesday) {
+                wednesdayCount++;
+            }
+            if(friday) {
+                fridayCount++;
+            }
+            if(saturday) {
+                saturdayCount++;
+            }
+            if(sunday) {
+                sundayCount++;
+            }
+
+        }
+        int max=Math.max(mondayCount,tuesdayCount);
+        int max2=Math.max(thursdayCount,wednesdayCount);
+        int max3=Math.max(fridayCount,saturdayCount);
+        int totalMax=Math.max(max,max2);
+        int totalMax2 =Math.max(totalMax,max3);
+        int totalMax3 = Math.max(totalMax2,sundayCount);
+        String day="";
+        if(totalMax3==mondayCount){
+            day += " monday";
+        }
+        if(totalMax3==tuesdayCount){
+            day += " tuesday";
+        }
+        if(totalMax3==thursdayCount){
+            day += " thursday";
+        }
+        if(totalMax3==wednesdayCount){
+            day += " wednesday";
+        }
+        if(totalMax3==fridayCount){
+            day += " friday";
+        }
+        if(totalMax3==saturdayCount){
+            day += " saturday";
+        }
+        if(totalMax3==sundayCount){
+            day += " sunday";
+        }
+       return day;
+    }
 
 }
