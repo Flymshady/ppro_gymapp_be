c50c69948acc58e1cb1d54970b07299d0ff83730
Blickmeister <ondrablick@seznam.cz>
2020-01-30 18:29:55 +0100

    Merge branch 'master' of https://github.com/Flymshady/ppro_gymapp_be
    auth updated

--- src/main/java/cz/ppro/gymapp/be/authentication/AuthenticationController.java
+++ src/main/java/cz/ppro/gymapp/be/authentication/AuthenticationController.java
@@ -1,4 +1,5 @@
 package cz.ppro.gymapp.be.authentication;
+import cz.ppro.gymapp.be.model.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -8,23 +9,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthenticationController {
 
+    private class AuthToSend {
+        private Long id;
+        private String roleName;
+        public AuthToSend(Long id, String roleName) {
+            this.id = id;
+            this.roleName = roleName;
+        }
+        public Long getId() {
+            return id;
+        }
+        public String getRoleName() {
+            return roleName;
+        }
+    }
+
     @CrossOrigin
     @GetMapping(path = "/basicauth")
-    public String helloWorldBean() {
+    public AuthToSend helloWorldBean() {
         //throw new RuntimeException("Ověření selhalo");
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if ((auth == null) || (auth.getPrincipal() == null)) {
             return null;
         }
-        return  auth.getAuthorities().toString();
-    }
-
-    @RequestMapping(value = "/current", method = RequestMethod.GET)
-    public String getUserPrincipal() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if ((auth == null) || (auth.getPrincipal() == null)) {
-            return null;
-        }
-        return  auth.getAuthorities().toString();
+        CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
+        return  new AuthToSend(user.getId(), user.getRoleString());
     }
 }
